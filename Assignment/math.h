f72211dffac722281464b93c5207f023bd749623
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-09-14 20:31:19 -0400

    changed O in Operator to lowercase

--- Assignment/math.h
+++ Assignment/math.h
@@ -24,6 +24,6 @@
 #define MATH_H_
 
 //Part of your documentation should be listing the valid inputs and outputs for the functions you create.
-int math(int num1, int num2, char Operator);
+int math(int num1, int num2, char operator);
 
 #endif /* MATH_H_ */
