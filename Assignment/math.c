fa2277de284ab30eb1aba8fdcc8105dbc33542a6
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-09-20 15:18:52 -0400

    added comments to math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,8 +1,12 @@
 /*
- * Andrew Hollabaugh
- * 9/14/19
+ * math.c
+ *
+ *  Created on: September 14, 2019
+ * 	Last Edited: September 20, 2019
+ *      Author: Andrew Hollabaugh
  */
 
+//Does the specified mathematical operation on two operands
 int math(int num1, int num2, char operator)
 {
     switch (operator)
@@ -10,13 +14,13 @@ int math(int num1, int num2, char operator)
         case '+': return num1 + num2;
         case '-': return num1 - num2;
         case '*': return num1 * num2;
-        case '/': return num2 == 0 ? 0 : num1 / num2;
-        case '%': return num2 == 0 ? 0 : num1 % num2;
-        case '<': return num2 > 0 ? num1 << num2 : num1;
-        case '>': return num2 > 0 ? num1 >> num2 : num1;
+        case '/': return num2 == 0 ? 0 : num1 / num2; //returns 0 if num2=0
+        case '%': return num2 == 0 ? 0 : num1 % num2; //returns 0 if num2=0
+        case '<': return num2 > 0 ? num1 << num2 : num1; //returns num1 if num2<=0
+        case '>': return num2 > 0 ? num1 >> num2 : num1; //returns num1 if num2<=0
         case '&': return num1 & num2;
         case '|': return num1 | num2;
         case '^': return num1 ^ num2;
-        case '~': return ~num1;
+        case '~': return ~num1; //ignores num2
     }
 }
