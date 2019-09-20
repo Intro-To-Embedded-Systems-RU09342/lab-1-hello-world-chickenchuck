41c498f3fe02719121f18a41d3169ec68459f6f7
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-09-20 15:01:07 -0400

    added edge cases

--- Assignment/math.c
+++ Assignment/math.c
@@ -10,10 +10,10 @@ int math(int num1, int num2, char operator)
         case '+': return num1 + num2;
         case '-': return num1 - num2;
         case '*': return num1 * num2;
-        case '/': return num1 / num2;
-        case '%': return num1 % num2;
-        case '<': return num1 << num2;
-        case '>': return num1 >> num2;
+        case '/': return num2 == 0 ? 0 : num1 / num2;
+        case '%': return num2 == 0 ? 0 : num1 % num2;
+        case '<': return num2 > 0 ? num1 << num2 : num1;
+        case '>': return num2 > 0 ? num1 >> num2 : num1;
         case '&': return num1 & num2;
         case '|': return num1 | num2;
         case '^': return num1 ^ num2;
